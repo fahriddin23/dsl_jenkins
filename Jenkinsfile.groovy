0ceaff18d7e6f0050844b1693cc76c4374a29898
fahriddin23 <fahriddin24@gmail.com>
2019-08-25 15:07:45 -0500

    done

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,8 +17,13 @@ pipeline{
             steps{
                 ws("tmp/"){
                 sh "pwd"
-                sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                sh "unzip terraform_0.12.7_linux_amd64.zip"
+                def exists fileExists 'terraform_0.12.7_linux_amd64.zip'
+                if (exists){
+                    "unzip -o terraform_0.12.7_linux_amd64.zip"
+                    } else {
+                        sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                    }
+                
                 sh "sudo mv terraform /bin"
 
                 }
